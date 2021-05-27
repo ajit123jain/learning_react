ae4326f99810ec97fdd0b497f893409c31ef40ac
ajit123jain <ajitkumarjain123@gmail.com>
2021-05-27 16:31:49 +0530

    pdf pagination

--- src/components/PoplifyPdf.js
+++ src/components/PoplifyPdf.js
@@ -5,10 +5,56 @@ import { Document, Page, pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.js`;
 
 export default function PoplifyPdf() {
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
+
   return (
-    <Document file={covidPdf}>
-      <Page pageNumber={1} />
+    <>
+    <Document
+      file={covidPdf}
+      onLoadSuccess={onDocumentLoadSuccess}
+    >
+      <Page pageNumber={pageNumber} />
     </Document>
+    <div>
+      <p>
+        Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+      </p>
+      <button
+        type="button"
+        disabled={pageNumber <= 1}
+        onClick={previousPage}
+      >
+        Previous
+      </button>
+      <button
+        type="button"
+        disabled={pageNumber >= numPages}
+        onClick={nextPage}
+      >
+        Next
+      </button>
+    </div>
+  </>
   );
 }
 
